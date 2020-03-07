5e4da902519a7634d284fe925b6791c89a03d214
Goteti Venkatesh <61575155+venkatesh80999@users.noreply.github.com>
2020-03-07 10:37:30 +0530

    Add files via upload

---
+++ com.venky.calculator/src/main/java/com/venky/calculator/Calculate.java
@@ -0,0 +1,7 @@
+package com.venky.calculator;
+
+public class Calculate {
+	public String operation (String a,String b){
+        return "Calculate class";
+    }
+}
---
+++ com.venky.calculator/src/main/java/com/venky/calculator/Divide.java
@@ -0,0 +1,16 @@
+package com.venky.calculator;
+
+public class Divide extends Calculate {
+    public String operation (String a,String b){
+        float x = Float.parseFloat(a);
+        float y = Float.parseFloat(b);
+        if(y == 0)
+            return "Cannot be divided by 0";
+        float ans = x/y;
+        int z = (int)ans;
+        if(ans - z == 0) 
+            return Integer.toString(z);
+        else 
+            return Float.toString(ans);
+    }
+}
